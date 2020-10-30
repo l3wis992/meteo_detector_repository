7f911f0a48131d829de328b5df208dac2751dc6d
Lewis <l3wis992@gmail.com>
2020-10-31 10:30:20 +1100

    0.4 + Changes to allow for live reading of analog input

--- SOFTPOT_read/SOFTPOT_read.ino
+++ SOFTPOT_read/SOFTPOT_read.ino
@@ -1,40 +1,105 @@
-/* MCP 3424 version 1.2 example sketch OneShotConversion
- Written by B@tto
- Contact : batto@hotmail.fr
+#include <Wire.h>
+#include <MCP342x.h>
 
- In this example, one conversion per second is performed on channel 1 and 16 bits resolution.
- A new conversion has to be initiated by the user
+/* Demonstrate the use of read() and convert(). If read() is called
+ * immediately after convert then the conversion will not have
+ * completed. Two approaches to avoid this problem are possible, use
+ * delay() or similar to wait a fixed amount of time, or to
+ * periodically read the device and check the config result.
  */
 
 
- #include <Wire.h>
- #include <MCP342x.h>
+// 0x68 is the default address for all MCP342x devices
+uint8_t address = 0x6E;
+MCP342x adc = MCP342x(address);
+
+// Configuration settings
+MCP342x::Config config(MCP342x::channel1, MCP342x::oneShot,
+           MCP342x::resolution14, MCP342x::gain1);
+
+// Configuration/status read back from the ADC
+MCP342x::Config status;
+
+// Inidicate if a new conversion should be started
+bool startConversion = false;
 
 
- uint8_t address = 0x6E;
- MCP342x adc(address);  // Declaration of MCP3424 with Address of I2C
+// LED details
+#ifdef LED_BUILTIN
+int led = LED_BUILTIN;
+#else
+int led = 13;
+#endif
+bool ledLevel = false;
+unsigned long timer;
+unsigned long oldTimer;
+unsigned long diff;
 
- long Voltage;
 
- void setup(){
-   Serial.begin(9600);
-   adc.convert(1,0,16,1); // Channel 1, 16 bits resolution, one-shot mode, amplifier gain = 1
+void setup(void)
+{
+  Serial.begin(9600);
+  Wire.begin();
 
+  // Enable power for MCP342x (needed for FL100 shield only)
+  pinMode(9, OUTPUT);
+  digitalWrite(9, HIGH);
   
+  pinMode(led, OUTPUT);
+    
+  // Reset devices
+  MCP342x::generalCallReset();
+  delay(1); // MC342x needs 300us to settle
+  
+  // Check device present
+  Wire.requestFrom(address, (uint8_t)1);
+  if (!Wire.available()) {
+    Serial.print("No device found at address ");
+    Serial.println(address, HEX);
+    while (1)
+      ;
   }
 
- }
+  // First time loop() is called start a conversion
+  startConversion = true;
+  timer = millis();
+}
 
- void loop(){
+unsigned long lastLedFlash = 0;
+void loop(void)
+{
+  long value = 0;
+  uint8_t err;
 
-   adc.NewConversion();    // New conversion is initiated
+  if (startConversion) {
+    Serial.println("Convert");
+    err = adc.convert(config);
+    if (err) {
+      Serial.print("Convert error: ");
+      Serial.println(err);
+    }
+    startConversion = false;
+  }
+  
+  err = adc.read(value, status);
+  if (!err && status.isReady()) { 
+    // For debugging purposes print the return value.
+    Serial.print("Value: ");
+    Serial.println(value);
+    Serial.print("Config: 0x");
+    Serial.println((int)config, HEX);
+    Serial.print("Convert error: ");
+    Serial.println(err);
+    startConversion = true;
+    diff = millis() - timer;
+    timer = millis();
+    Serial.println((String) "Diff = " + diff);
+  }
 
-   Voltage=MCP.Measure();  // Measure, note that the library waits for a complete conversion
-
-   Serial.print("Voltage = ");
-   Serial.print(Voltage);
-   Serial.println("uV");  // unit: microVolt
-
-   delay (1000);
-
- }
+  // Do other stuff here, such as flash an LED
+  if (millis() - lastLedFlash > 50) {
+    ledLevel = !ledLevel;
+    digitalWrite(led, ledLevel);
+    lastLedFlash = millis();
+  }
+}
--- WriteTests/WriteTests.ino
+++ WriteTests/WriteTests.ino
@@ -1,14 +1,25 @@
 // Lewis Hyman - Arduino Write Speed Tester
-double fileVersion = 0.31;
-const int chipSelect = BUILTIN_SDCARD; //declare SD socket
+double fileVersion = 0.40;
+
 
 #include <SD.h>
 #include <SPI.h>
+#include <MCP342x.h>
+#include <Wire.h>
+
+const int chipSelect = BUILTIN_SDCARD; //declare SD socket
+
+//Declare ADC
+uint8_t address = 0x6E;
+MCP342x adc = MCP342x(address);
+MCP342x::Config config(MCP342x::channel1, MCP342x::continous, MCP342x::resolution18, MCP342x::gain1);
+MCP342x::Config status;
+
 
 //methods:
 // testWrite(char 'type', int testLength, bool 'testRead')
 // type is a character, 'L', 'I', 'C' for Long, Integer or Character.
-// testlength is amount of data per test in kb
+// testlength is amount of data per test in kB
 // testRead will run a read speed test
 // returns total time for test.
 
@@ -16,15 +27,36 @@ File newFile;
 char serialInput = 0;
 String inputStr = "";
 
+boolean startConversion = false;
+
 void setup() {
   serialInit();
+  Wire.begin();
   SDinit();
-  testAll(5);
+
+  MCP342x::generalCallReset();
+  delay(1); //allow 300us to reset ADC
+  checkWire();
+  startConversion = true;
+
+  testWrite('A', 1000, false);
+
+
+  testAll(5000);
 }
 
 void loop() {
 }
 
+void checkWire() {
+  Wire.requestFrom(address, (uint8_t)1);
+  if (!Wire.available()) {
+    Serial.print("No device found at address ");
+    Serial.println(address, HEX);
+    while (1)
+      ;
+  }
+}
 void serialInit() {
   Serial.begin(9600);
   while (!Serial) {
@@ -33,10 +65,10 @@ void serialInit() {
 }
 
 void testAll(int writeSize) {
-  testWrite('C', writeSize, false);
-  testWrite('I', writeSize, false);
-  testWrite('L', writeSize, false);
-  testWrite('D', writeSize, false);
+  //testWrite('C', writeSize, false);
+  //testWrite('S', writeSize, false);
+  //testWrite('I', writeSize, false);
+  //testWrite('D', writeSize, false);
 
   Serial.println("\nAll tests completed.");
 }
@@ -55,21 +87,20 @@ void SDinit() {
 long testWrite(char type, int writeSize, boolean readTest) {
   newFile = SD.open("test.txt", FILE_WRITE);
 
-  Serial.print("\nTesting: ");
-
   int testLength = -1;
   int dataSize = -1;
 
   long initTimer = micros();
+  Serial.print((String)"\nWriting " + writeSize + "kB of datatype ");
   switch (type) {
 
-    case 'L': {
-        long randLong = random();
-        dataSize = sizeof(long);
+    case 'S': {
+        short randShort = random();
+        dataSize = sizeof(short);
         testLength = writeSize * 1000 / dataSize;
-        Serial.println((String)"Long (" + dataSize + " Bytes)");
+        Serial.println((String)"Short (" + dataSize + " Bytes)");
         for (int i = 0; i < testLength; i++) {
-          newFile.write(randLong);
+          newFile.write(randShort);
         }
         break;
       }
@@ -105,6 +136,43 @@ long testWrite(char type, int writeSize, boolean readTest) {
         for (int i = 0; i < testLength; i++) {
           newFile.write(randDouble);
         }
+
+
+      }
+    case 'A': {
+        dataSize = sizeof(long);
+        testLength = writeSize;
+        Serial.println((String)"Long (" + dataSize + " Bytes)");
+        int i = 0;
+
+        while (i < testLength) {
+          long value = 0;
+          uint8_t err;
+          if (startConversion) {
+            //Serial.println("Convert");
+            err = adc.convert(config);
+
+            if (err) {
+              Serial.print("Convert error: ");
+              Serial.println(err);
+            }
+            startConversion = false;
+
+          }
+
+          err = adc.read(value, status);
+          if (!err && status.isReady()) {
+            // For debugging purposes print the return value.
+            // Serial.println((String)"Value: " + value);
+            // Serial.print("Config: 0x");
+            // Serial.println((int)config, HEX);
+            // Serial.print((String)"Convert error: " + err);
+            // Serial.println(i);
+            startConversion = true;
+            newFile.write(value);
+            i++;
+          }
+        }
       }
   }
 
@@ -112,9 +180,9 @@ long testWrite(char type, int writeSize, boolean readTest) {
 
   float diffTimer = endTimer - initTimer;
   float timePerByte = diffTimer / (testLength * dataSize);
-  Serial.println((String)"Total # of tests run: " + testLength + " (" + (double)testLength * dataSize / 1000 + "Kb)");
-  Serial.println((String)"Total Time for test: " + diffTimer + " uS / " + diffTimer / 1000 + " mS / " + diffTimer / 1000000 + " S");
-  Serial.println((String)"Time per Write: " + (diffTimer / testLength) + " uS (" + timePerByte + " uS per byte /" + + timePerByte * 1000 "uS per kb)");
+  Serial.println((String)"Time: " + (diffTimer / 1000000) + "s, Total writes: " + testLength + " (" + (double)testLength * dataSize / 1000 + "kB)");
+  Serial.println((String)"Time per Write: " + (diffTimer / testLength) + " uS, (" + timePerByte + " uS per byte)");
+  Serial.println((String) "Transfer rate: "  + (1000000 / (timePerByte * 1000)) + "kB/S");
 
   if (newFile) {
     newFile.close();
@@ -126,7 +194,7 @@ long testWrite(char type, int writeSize, boolean readTest) {
   if (readTest) {
     if (writeSize <= 10) {
       if (newFile) {
-        Serial.println("Reading test2.txt:");
+        Serial.println("Reading test.txt:");
         while (newFile.available()) {
           Serial.write(newFile.read());
         }
@@ -145,4 +213,3 @@ long testWrite(char type, int writeSize, boolean readTest) {
   return diffTimer;
 
 }
-
